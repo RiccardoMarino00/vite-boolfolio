532fc4329b751e0d1d968aa4fd9ce5dbddbac9d6
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-06-12 14:53:13 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+export default {
+  data() {
+    return {
+      message: 'Ciao Mondo'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1> {{ message }} </h1>
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+</template>
+
+<style></style>
