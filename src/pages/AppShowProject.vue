b6c78b4e0dcc84d5c99056d143396763e238f26e
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-06-15 15:08:49 +0200

    Added: parametric route and added project info in show

--- src/pages/AppShowProject.vue
+++ src/pages/AppShowProject.vue
@@ -1,5 +1,28 @@
 <template>
-    <h1>{{ $route.params.slug }}</h1>
+    <template v-if="project">
+        <h1>{{ $route.params.slug }}</h1>
+        <p>{{ project.title }}</p>
+        <div class="col-6 bordi riga-flex">
+            <div class="col-6 col-title">
+                <h2 class="color-white"> {{ project.title }}</h2>
+            </div>
+            <div class="col-6">
+                <p> {{ project.content }}</p>
+                <a class="link" href="{{ project.link }}">Go to Repository&#10066;</a>
+                <router-link :to="{ name: 'showProject', params: { slug: project.slug } }">Read more</router-link>
+
+                <ul v-if="project.technologies">
+                    <li v-for="technology in project.technologies">
+                        {{ technology.name }}
+                    </li>
+                </ul>
+                <ul v-if="project.type">
+                    <li>{{ project.type.name }}</li>
+                </ul>
+            </div>
+        </div>
+    </template>
+
 
 </template>
 
